ed3580ba373c1cf6692fd91e0e93e451fffabde9
NohYeonggwon <shdudrnjs0@naver.com>
2022-10-24 18:37:26 +0900

    10/24 18:30
    베너 이미지 변경 및 레시피 페이지

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import Myr from './components/Myr';
 import Myr2 from './components/Myr2';
 import ImgUpload from './components/ImgUpload';
 import ImgUpload2 from './components/ImgUpload2';
+import Recipe from './components/Recipe';
+import Recipe2 from './components/Recipe2';
 import {Routes, Route, BrowserRouter} from 'react-router-dom';
 
 function App() {
@@ -18,11 +20,13 @@ function App() {
         <Header></Header>
         <Nav></Nav>
         <Routes>
-          <Route path='/' element={<><Content1></Content1><Content2></Content2></>}></Route>
-          <Route path='/myr' element={<Myr></Myr>}></Route>
-          <Route path='/myr2' element={<Myr2></Myr2>}></Route>
-          <Route path='/img' element={<ImgUpload></ImgUpload>}></Route>
-          <Route path='/img2' element={<ImgUpload2></ImgUpload2>}></Route>
+          <Route exact path='/' element={<><Content1></Content1><Content2></Content2></>}></Route>
+          <Route exact path='/myr' element={<Myr></Myr>}></Route>
+          <Route exact path='/myr2' element={<Myr2></Myr2>}></Route>
+          <Route exact path='/img' element={<ImgUpload></ImgUpload>}></Route>
+          <Route exact path='/img2' element={<ImgUpload2></ImgUpload2>}></Route>
+          <Route exact path='/recipe' element={<Recipe></Recipe>}></Route>
+          <Route exact path='/recipe/:id' element={<Recipe2></Recipe2>}></Route>
         </Routes>
         <Footer></Footer>
       </div>
